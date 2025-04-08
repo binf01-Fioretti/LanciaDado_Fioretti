8ccbef886f8587ba0b43921e27ae5a5e73d48a6d
Fioretti Daniele <fiorettidaniele9@gmail.com>
2025-04-08 12:45:51 +0200

    Progetto creato

---
+++ app/src/main/java/com/example/lanciadado_fioretti_5binf/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.lanciodado_5binf
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.lanciadado_fioretti_5binf.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        var btmroll = findViewById<Button>(R.id.buttonLancia)
+        btmroll.setOnClickListener(View.OnClickListener {
+            var mioToast = Toast.makeText(this, "DADO LANCIATO", Toast.LENGTH_LONG )
+            mioToast.show()
+        })
+        var txtTitle = findViewById<TextView>(R.id.textViewTitle)
+        var imgView = findViewById<ImageView>(R.id.ImageViewDado)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LanciaDado_Fioretti_5BINF"
+include(":app")
