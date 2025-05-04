1ff1b08a9b1de2e7c16584daf2bbff8230451bd7
binf01-Fioretti <fiorettidaniele9@gmail.com>
2025-05-04 13:44:40 +0200

    Recuperati gli elementi

--- app/src/main/java/com/example/lanciadado_fioretti_5binf/secondActivity.kt
+++ app/src/main/java/com/example/lanciadado_fioretti_5binf/secondActivity.kt
@@ -1,6 +1,11 @@
 package com.example.lanciadado_fioretti_5binf
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +17,19 @@ class secondActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_second)
 
+        val btnRisultato = findViewById<Button>(R.id.buttonRisultato)
+        btnRisultato.setOnClickListener {
+            val mioToast = Toast.makeText(this, "RISULTATO", Toast.LENGTH_LONG)
+            mioToast.show()
+        }
+        var txtNumber = findViewById<TextView>(R.id.textViewNumber)
+        var imgView = findViewById<ImageView>(R.id.ImageViewDado)
         val mioRandom = intent.getIntExtra("NUMERO", -1)
+        }
+
+        private fun lanciaIntent(mioRandom: Int){
+            val mioIntent = Intent(this, secondActivity::class.java)
+            mioIntent.putExtra("NUMERO", mioRandom)
+            startActivity(mioIntent)
         }
     }
